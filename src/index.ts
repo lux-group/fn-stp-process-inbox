2b1b12ebf8a12c43a2016027f0b5488a9ae2f43d
Justin Hopkins <justin@commoncode.io>
2020-03-10 11:26:05 +1100

    Configure esline and auto fix

--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,71 @@
-import {
-  S3
-} from 'aws-sdk'
-import async from 'async'
+import { S3 } from "aws-sdk";
+import async from "async";
 
-import {
-  simpleParser
-} from 'mailparser'
+import { simpleParser } from "mailparser";
 
-import {
-  INBOX_S3_BUCKET_NAME,
-  DATA_S3_BUCKET_NAME
-} from './config'
+import { INBOX_S3_BUCKET_NAME, DATA_S3_BUCKET_NAME } from "./config";
 
-const s3 = new S3()
+const s3 = new S3();
 
 export function handler(event: any, context: any, callback: any) {
-  const { ses } = event.Records[0]
-  const { timestamp } = ses.mail
+  const { ses } = event.Records[0];
+  const { timestamp } = ses.mail;
   //console.log('SES Notification:\n', JSON.stringify(ses, null, 2))
 
   interface Header {
-    name: string
-    value: string
+    name: string;
+    value: string;
   }
-  const { headers } : {headers: Header[]}  = ses.mail
-  const origHeaders = headers.filter(h => h.name === 'X-OriginatorOrg')
+  const { headers }: { headers: Header[] } = ses.mail;
+  const origHeaders = headers.filter(h => h.name === "X-OriginatorOrg");
 
   if (origHeaders.length == 0 || !origHeaders[0].value) {
-    callback('X-OriginatorOrg missing from email.')
-    return
+    callback("X-OriginatorOrg missing from email.");
+    return;
   }
-  const origOrg = origHeaders[0].value
+  const origOrg = origHeaders[0].value;
 
   const getS3Key = (fileName: string) =>
-    `${timestamp.replace(/-/g, '/')}_${origOrg}/${fileName}`
+    `${timestamp.replace(/-/g, "/")}_${origOrg}/${fileName}`;
 
-  const getAttachments = (data: any) => data.attachments.filter((a: any) => a.contentDisposition === 'attachment')
+  const getAttachments = (data: any) =>
+    data.attachments.filter((a: any) => a.contentDisposition === "attachment");
 
   async.waterfall(
     [
       function getRawEmail(next: any) {
         console.log(
-          `Getting raw email ${
-            ses.mail.messageId
-          } from ${INBOX_S3_BUCKET_NAME} for ${origOrg}`
-        )
-        s3.getObject({
+          `Getting raw email ${ses.mail.messageId} from ${INBOX_S3_BUCKET_NAME} for ${origOrg}`
+        );
+        s3.getObject(
+          {
             Bucket: INBOX_S3_BUCKET_NAME,
             Key: ses.mail.messageId
           },
           next
-        )
+        );
       },
       function parseEmail(response: any, next: any) {
-        console.log('Parsing raw email...')
-        simpleParser(response.Body, {}, next)
+        console.log("Parsing raw email...");
+        simpleParser(response.Body, {}, next);
       },
       function writeMetadata(data: any, next: any) {
-        console.log('Writing Attachments...')
-        getAttachments(data).forEach(
-          (a: any) => {
-            s3.putObject({
-                Bucket: DATA_S3_BUCKET_NAME,
-                Key: getS3Key(a.filename),
-                Body: a.content
-              },
-              (err: any, data: any) => {
-                if (err) {
-                  next(err)
-                }
+        console.log("Writing Attachments...");
+        getAttachments(data).forEach((a: any) => {
+          s3.putObject(
+            {
+              Bucket: DATA_S3_BUCKET_NAME,
+              Key: getS3Key(a.filename),
+              Body: a.content
+            },
+            (err: any, data: any) => {
+              if (err) {
+                next(err);
               }
-            )
-          })
-        console.log('Writing Metadata...')
+            }
+          );
+        });
+        console.log("Writing Metadata...");
         const metaData = {
           date: data.date,
           subject: data.subject,
@@ -86,23 +79,24 @@ export function handler(event: any, context: any, callback: any) {
             size: a.size,
             s3key: getS3Key(a.filename)
           }))
-        }
-        s3.putObject({
+        };
+        s3.putObject(
+          {
             Bucket: DATA_S3_BUCKET_NAME,
-            Key: getS3Key('_meta.json'),
+            Key: getS3Key("_meta.json"),
             Body: JSON.stringify(metaData, null, 2)
           },
           next
-        )
+        );
       }
     ],
-    function (err) {
+    function(err) {
       if (err) {
-        console.error(err)
-        callback(err)
+        console.error(err);
+        callback(err);
       } else {
-        callback(null, 'success')
+        callback(null, "success");
       }
     }
-  )
+  );
 }
