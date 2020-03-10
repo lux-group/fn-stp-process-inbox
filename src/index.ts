016aa927a77a80d1bf88360c07621ae3add1fa9f
Justin Hopkins <justin@commoncode.io>
2020-03-10 16:29:45 +1100

    Convert to async and fix all type warnings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { S3 } from "aws-sdk";
 import { Attachment, ParsedMail } from "mailparser";
-import async from "async";
 
 import { simpleParser } from "mailparser";
 
@@ -8,100 +7,109 @@ import { INBOX_S3_BUCKET_NAME, DATA_S3_BUCKET_NAME } from "./config";
 
 const s3 = new S3();
 
-export function handler(event: any, context: any, callback: any) {
+interface Header {
+  name: string;
+  value: string;
+}
+interface SESEventRecord {
+  ses: { mail: { headers: Header[]; timestamp: string; messageId: string } };
+}
+interface SESEvent {
+  Records: SESEventRecord[];
+}
+export const handler = async (event: SESEvent): Promise<string> => {
+  let rawEmailResponse = null;
+  let data: ParsedMail;
+  let attachments: Attachment[];
+
   const { ses } = event.Records[0];
   const { timestamp } = ses.mail;
-  //console.log('SES Notification:\n', JSON.stringify(ses, null, 2))
 
-  interface Header {
-    name: string;
-    value: string;
-  }
   const { headers }: { headers: Header[] } = ses.mail;
   const origHeaders = headers.filter(h => h.name === "X-OriginatorOrg");
 
   if (origHeaders.length == 0 || !origHeaders[0].value) {
-    callback("X-OriginatorOrg missing from email.");
-    return;
+    return "Error: X-OriginatorOrg missing from email.";
   }
   const origOrg = origHeaders[0].value;
 
   const getS3Key = (fileName: string): string =>
     `${timestamp.replace(/-/g, "/")}_${origOrg}/${fileName}`;
 
-  const getAttachments = (data: ParsedMail): Attachment[] =>
-    data?.attachments
+  console.log(
+    `Getting raw email ${ses.mail.messageId} from ${INBOX_S3_BUCKET_NAME} for ${origOrg}`
+  );
+  try {
+    rawEmailResponse = await s3
+      .getObject({ Bucket: INBOX_S3_BUCKET_NAME, Key: ses.mail.messageId })
+      .promise();
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  console.log("Parsing raw email...");
+  try {
+    data = await simpleParser(
+      Buffer.from(rawEmailResponse?.Body?.toString() || "")
+    );
+
+    attachments = data?.attachments
       ? data.attachments.filter(
           (a: Attachment) => a.contentDisposition === "attachment"
         )
       : [];
+    console.log(`Attachments found: ${JSON.stringify(attachments.length)}`);
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 
-  async.waterfall(
-    [
-      function getRawEmail(next: any): void {
-        console.log(
-          `Getting raw email ${ses.mail.messageId} from ${INBOX_S3_BUCKET_NAME} for ${origOrg}`
-        );
-        s3.getObject(
-          {
-            Bucket: INBOX_S3_BUCKET_NAME,
-            Key: ses.mail.messageId
-          },
-          next
-        );
-      },
-      function parseEmail(response: any, next: any): void {
-        console.log("Parsing raw email...");
-        simpleParser(response.Body, {}, next);
-      },
-      function writeMetadata(data: any, next: any): void {
-        console.log("Writing Attachments...");
-        getAttachments(data).forEach((a: any) => {
-          s3.putObject(
-            {
-              Bucket: DATA_S3_BUCKET_NAME,
-              Key: getS3Key(a.filename),
-              Body: a.content
-            },
-            (err: any) => {
-              if (err) {
-                next(err);
-              }
-            }
-          );
-        });
-        console.log("Writing Metadata...");
-        const metaData = {
-          date: data.date,
-          subject: data.subject,
-          from: data.from.value[0].address,
-          text: data.text,
-          textAsHtml: data.textAsHtml,
-          attachments: getAttachments(data).map((a: any) => ({
-            contentType: a.contentType,
-            contentDisposition: a.contentDisposition,
-            filename: a.filename,
-            size: a.size,
-            s3key: getS3Key(a.filename)
-          }))
-        };
-        s3.putObject(
-          {
+  console.log("Writing Attachments...");
+  try {
+    await Promise.all(
+      attachments.map(async (a: Attachment) => {
+        const putResult = await s3
+          .putObject({
             Bucket: DATA_S3_BUCKET_NAME,
-            Key: getS3Key("_meta.json"),
-            Body: JSON.stringify(metaData, null, 2)
-          },
-          next
-        );
-      }
-    ],
-    function(err) {
-      if (err) {
-        console.error(err);
-        callback(err);
-      } else {
-        callback(null, "success");
-      }
-    }
-  );
-}
+            Key: getS3Key(a.filename || ""),
+            Body: a.content
+          })
+          .promise();
+        console.log(JSON.stringify(putResult));
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+
+  console.log("Writing Metadata...");
+  try {
+    const metaData = {
+      date: data.date,
+      subject: data.subject,
+      from: data.from.value[0].address,
+      text: data.text,
+      textAsHtml: data.textAsHtml,
+      attachments: attachments.map((a: Attachment) => ({
+        contentType: a.contentType,
+        contentDisposition: a.contentDisposition,
+        filename: a.filename,
+        size: a.size,
+        s3key: getS3Key(a.filename || "")
+      }))
+    };
+    await s3
+      .putObject({
+        Bucket: DATA_S3_BUCKET_NAME,
+        Key: getS3Key("_meta.json"),
+        Body: JSON.stringify(metaData, null, 2)
+      })
+      .promise();
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+  return "Finished processing.";
+};
